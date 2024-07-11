045222d434e9c704605ee69650481f6ceb62b2c7
Amin Halvaei <aminhalvaei80@gmail.com>
2024-07-11 16:46:28 +0330

    some actions like +-*/ are working now

--- src/assets/components/ActionButton.jsx
+++ src/assets/components/ActionButton.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 const labelMapper = {
     '=': 'result',
@@ -6,12 +6,27 @@ const labelMapper = {
     '-': 'subtract',
     '*': 'multiply',
     '/': 'divide',
+    '%': 'percent',
     'C': 'clear',
     'T': 'toggle',
+    '.': 'dot'
 }
+
+const operations = {
+    add: 0,
+    subtract: 1,
+    multiply: 2,
+    divide: 3,
+    percent: 4,
+    toggle: 5, 
+    clear: 6,
+    dot: 7,
+    result: 8
+};
 
 export default function ActionButton({ label, className }) {
   const dispatch = useDispatch();
+  const operationVisible = useSelector(state => state.operationVisible)
 
   function actionHandler() {
     dispatch({ type: "action/" + labelMapper[label] });
@@ -19,7 +34,7 @@ export default function ActionButton({ label, className }) {
   return (
     <button
       onClick={actionHandler}
-      className={`flex justify-center items-center h-16 text-2xl text-white rounded-full ${className}`}
+      className={`flex justify-center items-center h-16 text-2xl text-white rounded-full ${className}  ${operationVisible === operations[labelMapper[label]] && labelMapper[label] !== null ? 'bg-green-400' : ''}`}
     >
       {label}
     </button>
--- src/assets/store/index.js
+++ src/assets/store/index.js
@@ -5,7 +5,11 @@ const operations = Object.freeze({
   subtract: 1,
   multiply: 2,
   divide: 3,
-  toggle: 4,
+  percent: 4,
+  toggle: 5,
+  clear: 6,
+  dot: 7,
+  result: 8
 });
 
 function contentReducer(
@@ -54,25 +58,29 @@ function contentReducer(
       }
     case "action/add":
       return {
-        cumContent: state.cumContent + state.displayContent,
+        cumContent: state.displayContent,
+        displayContent: state.displayContent,
         operation: operations.add,
         operationVisible: operations.add,
       };
     case "action/subtract":
       return {
-        cumContent: state.cumContent - state.displayContent,
+        cumContent: state.displayContent,
+        displayContent: state.displayContent,
         operation: operations.subtract,
         operationVisible: operations.subtract,
       };
     case "action/multiply":
       return {
-        cumContent: state.cumContent * state.displayContent,
+        cumContent: state.displayContent,
+        displayContent: state.displayContent,
         operation: operations.multiply,
         operationVisible: operations.multiply,
       };
     case "action/divide":
       return {
-        cumContent: state.cumContent / state.displayContent,
+        cumContent: state.displayContent,
+        displayContent: state.displayContent,
         operation: operations.divide,
         operationVisible: operations.divide,
       };
@@ -81,118 +89,173 @@ function contentReducer(
         cumContent: 0,
         displayContent: 0,
         operation: null,
+        operationVisible: null,
       };
     case "action/toggle":
       return {
         cumContent: state.cumContent * -1,
         displayContent: state.displayContent * -1,
+        operation: null,
+        operationVisible: null,
       };
     case "content/1":
-      if (state.operationVisible) {
+      if (state.operationVisible !== null) {
         return {
           displayContent: 1,
+          cumContent: state.cumContent,
+          operation : state.operation,
           operationVisible: null,
         };
       } else {
         let updatedDisplay = state.displayContent + "1";
         return {
           displayContent: Number(updatedDisplay),
+          cumContent: state.cumContent,
+          operation : state.operation,
         };
       }
     case "content/2":
-      if (state.operationVisible) {
+      if (state.operationVisible !== null) {
         return {
           displayContent: 2,
+          cumContent: state.cumContent,
+          operation : state.operation,
           operationVisible: null,
         };
       } else {
         let updatedDisplay = state.displayContent + "2";
         return {
           displayContent: Number(updatedDisplay),
+          cumContent: state.cumContent,
+          operation : state.operation,
         };
       }
     case "content/3":
-      if (state.operationVisible) {
+      if (state.operationVisible !== null) {
         return {
           displayContent: 3,
+          cumContent: state.cumContent,
+          operation : state.operation,
           operationVisible: null,
         };
       } else {
         let updatedDisplay = state.displayContent + "3";
         return {
           displayContent: Number(updatedDisplay),
+          cumContent: state.cumContent,
+          operation : state.operation,
         };
       }
     case "content/4":
-      if (state.operationVisible) {
+      if (state.operationVisible !== null) {
         return {
           displayContent: 4,
+          cumContent: state.cumContent,
+          operation : state.operation,
           operationVisible: null,
         };
       } else {
         let updatedDisplay = state.displayContent + "4";
         return {
           displayContent: Number(updatedDisplay),
+          cumContent: state.cumContent,
+          operation : state.operation,
         };
       }
     case "content/5":
-      if (state.operationVisible) {
+      if (state.operationVisible !== null) {
         return {
           displayContent: 5,
+          cumContent: state.cumContent,
+          operation : state.operation,
           operationVisible: null,
         };
       } else {
         let updatedDisplay = state.displayContent + "5";
         return {
           displayContent: Number(updatedDisplay),
+          cumContent: state.cumContent,
+          operation : state.operation,
         };
       }
     case "content/6":
-      if (state.operationVisible) {
+      if (state.operationVisible !== null) {
         return {
           displayContent: 6,
+          cumContent: state.cumContent,
+          operation : state.operation,
           operationVisible: null,
         };
       } else {
         let updatedDisplay = state.displayContent + "6";
         return {
           displayContent: Number(updatedDisplay),
+          cumContent: state.cumContent,
+          operation : state.operation,
         };
       }
     case "content/7":
-      if (state.operationVisible) {
+      if (state.operationVisible !== null) {
         return {
           displayContent: 7,
+          operation : state.operation,
           operationVisible: null,
+          cumContent: state.cumContent,
         };
       } else {
         let updatedDisplay = state.displayContent + "7";
         return {
           displayContent: Number(updatedDisplay),
+          cumContent: state.cumContent,
+          operation : state.operation,
         };
       }
     case "content/8":
-      if (state.operationVisible) {
+      if (state.operationVisible !== null) {
         return {
           displayContent: 8,
+          operation : state.operation,
           operationVisible: null,
+          cumContent: state.cumContent,
         };
       } else {
         let updatedDisplay = state.displayContent + "8";
         return {
           displayContent: Number(updatedDisplay),
+          cumContent: state.cumContent,
+          operation : state.operation,
         };
       }
     case "content/9":
-      if (state.operationVisible) {
+      if (state.operationVisible !== null) {
         return {
           displayContent: 9,
+          cumContent: state.cumContent,
+          operation : state.operation,
           operationVisible: null,
         };
       } else {
         let updatedDisplay = state.displayContent + "9";
         return {
           displayContent: Number(updatedDisplay),
+          cumContent: state.cumContent,
+          operation : state.operation,
+        };
+      }
+    case "content/0":
+      if (state.operationVisible !== null) {
+        return {
+          displayContent: 0,
+          cumContent: state.cumContent,
+          operation : state.operation,
+          operationVisible: null,
+        };
+      } else {
+        let updatedDisplay = state.displayContent + "0";
+        return {
+          displayContent: Number(updatedDisplay),
+          cumContent: state.cumContent,
+          operation : state.operation,
         };
       }
     default:
